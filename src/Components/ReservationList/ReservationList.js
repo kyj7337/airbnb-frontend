3bf18e94a4c711ba575a4e0f4c76048fce2f34ef
bclef2525 <bclef25@gmail.com>
2019-11-22 12:57:25 +0900

    데이터 얀결 완료

--- src/Components/ReservationList/ReservationList.js
+++ src/Components/ReservationList/ReservationList.js
@@ -1,28 +1,65 @@
 import React, { Component } from "react";
 
 import "./ReservationList.scss";
+
 export class ReservationList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: ""
+    };
+  }
+  handleStatus = () => {
+    fetch("http://10.58.1.221:8001/reservation/confirm", {
+      method: "post",
+      headers: {
+        Authorization:
+          "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoyNDh9.QBOVJMpQS_PUbX-hDecNlC3vvTiCrm8kc6aRAn3h9gc"
+      },
+      body: JSON.stringify({ reservation_id: this.props.data.reservation_id })
+    });
+    this.setState({});
+    alert("예약이 완료 되었습니다.");
+  };
+
   render() {
+    const { data } = this.props;
     return (
       <>
         <div className="hostadmin-rooms-list">
-          <div className="hostadmin-image"></div>
+          <div
+            className="hostadmin-image"
+            style={{ backgroundImage: `url(${data.room_picture})` }}
+          ></div>
           <div className="hostadmin-room-info-container">
-            <div className="hostadmin-room-name-container">Mongsang</div>
+            <div className="hostadmin-room-name-container">
+              {data.room_title}
+            </div>
             <div className="hostadmin-reservation-date">
+              <br />
               <br />
               예약날짜:
               <br />
               <br />
-              2019-11-22 ~ 2019-11-27
+              {data.check_in_year}- {data.check_in_month}- {data.check_in_day} ~
+              {data.check_out_year}- {data.check_out_month}-{data.check_out_day}
             </div>
             <div className="hostadmin-guest-name">
               <br />
-              게스트: 조인호 외 1명
+              게스트: {data.guest_name} 외 {data.guest_number - 1}명
             </div>
             <div className="hostadmin-reservation-status">
               <br />
-              예약현황: 예약신청
+              예약현황:{" "}
+              <div className={data.is_confirmed ? "status" : ""}>
+                <br />
+                {data.is_confirmed ? "예약완료" : "예약신청"}
+              </div>
+              <br />
+              <button className="hostadmin-cancle">예약거부</button>
+              <button className="hostadmin-accept" onClick={this.handleStatus}>
+                &nbsp;예약확인
+              </button>
             </div>
           </div>
         </div>
--- src/Pages/HostAdmin/HostAdmin.js
+++ src/Pages/HostAdmin/HostAdmin.js
@@ -1,23 +1,48 @@
 import React, { Component } from "react";
-import Reservation from "../../Components/ReservationList";
-import "./HostAdmin.scss";
 import ReservationList from "../../Components/ReservationList";
+import Header from "../../Components/Header/DetailHeader";
+import "./HostAdmin.scss";
 export class HostAdmin extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: []
+    };
+  }
+  componentDidMount() {
+    fetch("http://10.58.1.221:8001/reservation/host", {
+      method: "get",
+      headers: {
+        Authorization:
+          "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoyNDh9.QBOVJMpQS_PUbX-hDecNlC3vvTiCrm8kc6aRAn3h9gc"
+      }
+    })
+      .then(res => {
+        return res.json();
+      })
+      .then(res => {
+        this.setState({ data: res.reservation_data }, () => {
+          console.log(this.state.data);
+        });
+      });
+  }
   render() {
+    const list = this.state.data.map(el => {
+      return <ReservationList data={el} />;
+    });
     return (
-      <div className="hostadmin-page">
-        <div className="hostadmin-image-container">
-          <div className="hostadmin-text">예약관리</div>
-          <div className="hostadmin-image"></div>
+      <>
+        <Header />
+        <div className="hostadmin-page">
+          <div className="hostadmin-image-container">
+            <div className="hostadmin-text">예약관리</div>
+            <div className="hostadmin-image"></div>
+          </div>
+          <div className="hostadmin-rooms-list-container">
+            {this.state.data && list}
+          </div>
         </div>
-        <div className="hostadmin-rooms-list-container">
-          <ReservationList />
-          <ReservationList />
-          <ReservationList />
-          <ReservationList />
-          <ReservationList />
-        </div>
-      </div>
+      </>
     );
   }
 }
